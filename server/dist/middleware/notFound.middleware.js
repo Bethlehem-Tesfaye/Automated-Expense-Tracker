2f97be98807035cf165f639050b0edb99146c3ff
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2026-02-27 04:46:39 +0400

    fix:Remove dist from repo and ignore build output

--- server/dist/middleware/notFound.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-function notFound(req, _res, next) {
-    const error = new Error(`Route not found: ${req.method} ${req.originalUrl}`);
-    error.statusCode = 404;
-    error.isOperational = true;
-    next(error);
-}
--- server/dist/templates/emails/VerifyEmail.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VerifyEmail = VerifyEmail;
-const jsx_runtime_1 = require("react/jsx-runtime");
-const components_1 = require("@react-email/components");
-function VerifyEmail({ name, verifyUrl }) {
-    return ((0, jsx_runtime_1.jsxs)(components_1.Html, { children: [(0, jsx_runtime_1.jsx)(components_1.Head, {}), (0, jsx_runtime_1.jsx)(components_1.Body, { style: { fontFamily: "Arial, sans-serif" }, children: (0, jsx_runtime_1.jsxs)(components_1.Container, { children: [(0, jsx_runtime_1.jsxs)(components_1.Text, { children: ["Hello ", name, ","] }), (0, jsx_runtime_1.jsx)(components_1.Text, { children: "Please verify your email address:" }), (0, jsx_runtime_1.jsx)(components_1.Button, { href: verifyUrl, children: "Verify Email" })] }) })] }));
-}
