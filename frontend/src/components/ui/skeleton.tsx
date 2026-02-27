df5f8702d91bd8e2cd5ee979be31159483acd596
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2026-02-27 04:47:38 +0400

    fix: update background gradient in Skeleton component

--- frontend/src/components/ui/skeleton.tsx
+++ frontend/src/components/ui/skeleton.tsx
@@ -5,7 +5,7 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="skeleton"
       className={cn(
-        "relative overflow-hidden rounded-md bg-gray-200/90 before:absolute before:inset-0 before:-translate-x-full before:animate-[skeleton-shimmer_1.4s_ease-in-out_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/70 before:to-transparent before:content-[''] motion-reduce:before:animate-none",
+        "relative overflow-hidden rounded-md bg-gray-200/90 before:absolute before:inset-0 before:-translate-x-full before:animate-[skeleton-shimmer_1.4s_ease-in-out_infinite] before:bg-linear-to-r before:from-transparent before:via-white/70 before:to-transparent before:content-[''] motion-reduce:before:animate-none",
         className,
       )}
       {...props}
